144cabc685456b28a03c8533aef8b61c9716a4d4
Ryan Couvillon <rcouvillon@islander.tamucc.edu>
2026-04-20 00:16:41 +0000

    Fix CI test

---
+++ your_tests.py
@@ -0,0 +1,11 @@
+from calculator_adapter import run
+
+def test_addition():
+	result = run(["2", "+", "3"])
+	asset "5" in result
+
+def test_multiplication():
+	result = run(["4", "*", "5"])
+	asset "20" in result
+
+print("All tests passed!")
