aa10b2b6b8606a907042dac839c72952735734ca
Ryan Couvillon <rcouvillon@islander.tamucc.edu>
2026-04-20 00:12:06 +0000

    Finished assignment 6

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -2,9 +2,12 @@
 from calculator_adapter import run
 
 
-### ADD AT LEAST TWO TESTS HERE!
+def test_addition():
+	result = run(["2", "+", "3"])
+	assert "5" in result
 
-
-###
+def test_multiplication():
+	result = run(["4", "*", "5"])
+	assert "20" in result
 
 print("All tests passed!")
